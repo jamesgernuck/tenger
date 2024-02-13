c0d91e649609493c3e19e1bf99bdc961dd598f41
jamesgernuck <jamesgernuck@users.noreply.github.com>
2024-02-13 17:44:36 -0600

    commit

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2014 Chris Wilson
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 window.audioContext = window.audioContext || window.webkitaudioContext;
 
 var audioContext = null;
@@ -50,7 +26,7 @@ var playing = 0;
 var rotate = 0;
 var settings_arr = [];
 var input_arr = [];
-settings_defaults = `#00ffbf,0,1,10,#c8c8c8,0,10`
+settings_defaults = `#c8c8c8,200,1,10,#00ffbf,10,10`
 
 window.onload = function() {
 	audioContext = new AudioContext();
